eeacc3b7da02589617506a7901cfd53086cca5d3
projectmyguru <project.myguru@gmail.com>
2017-03-12 21:39:20 +0700

    tes map

--- app/src/main/java/com/example/myguru/MapsActivity.java
+++ app/src/main/java/com/example/myguru/MapsActivity.java
@@ -4,27 +4,34 @@ import android.Manifest;
 import android.content.pm.PackageManager;
 import android.location.Location;
 import android.support.annotation.NonNull;
+import android.support.annotation.Nullable;
 import android.support.v4.app.ActivityCompat;
 import android.support.v4.app.FragmentActivity;
 import android.os.Bundle;
 
+import com.google.android.gms.common.ConnectionResult;
+import com.google.android.gms.common.api.GoogleApiClient;
 import com.google.android.gms.maps.CameraUpdateFactory;
 import com.google.android.gms.maps.GoogleMap;
 import com.google.android.gms.maps.OnMapReadyCallback;
 import com.google.android.gms.maps.SupportMapFragment;
 import com.google.android.gms.maps.UiSettings;
 import com.google.android.gms.maps.model.LatLng;
+import com.google.android.gms.location.LocationServices;
 import com.google.android.gms.maps.model.MarkerOptions;
 
 import static android.Manifest.permission.ACCESS_FINE_LOCATION;
 
-public class MapsActivity extends FragmentActivity implements OnMapReadyCallback {
-
+public class MapsActivity extends FragmentActivity implements OnMapReadyCallback, GoogleApiClient.ConnectionCallbacks, GoogleApiClient.OnConnectionFailedListener
+{
     private GoogleMap mMap;
     private static final int REQUEST_ACCESS_FINE_LOCATION = 0;
+    private static final int REQUEST_GET_LOCATION = 1;
+    protected GoogleApiClient mGoogleApiClient;
 
     @Override
-    protected void onCreate(Bundle savedInstanceState) {
+    protected void onCreate(Bundle savedInstanceState)
+    {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_maps);
         // Obtain the SupportMapFragment and get notified when the map is ready to be used.
@@ -44,29 +51,70 @@ public class MapsActivity extends FragmentActivity implements OnMapReadyCallback
      * installed Google Play services and returned to the app.
      */
     @Override
-    public void onMapReady(GoogleMap googleMap) {
+    public void onMapReady(GoogleMap googleMap)
+    {
         mMap = googleMap;
         getLocationPermission();
 
         UiSettings setting = mMap.getUiSettings();
         setting.setZoomControlsEnabled(true);
-        setting.setMapToolbarEnabled(true);
 
-        // Add a marker in Sydney and move the camera
-        double lat = myLocation.getLatitude();
-        double lng = myLocation.getLongitude();
-        LatLng currentLocation = new LatLng(lat, lng);
-        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(currentLocation,15));
+        mGoogleApiClient = new GoogleApiClient.Builder(this)
+                .addConnectionCallbacks(this)
+                .addOnConnectionFailedListener(this)
+                .addApi(LocationServices.API)
+                .build();
+
+        mGoogleApiClient.connect();
+    }
+
+    @Override
+    public void onConnected(Bundle bundle)
+    {
+        Location mLastLocation = LocationServices.FusedLocationApi.getLastLocation(mGoogleApiClient);
+        LatLng loc;
+
+        if (mLastLocation != null)
+        {
+            double lat = mLastLocation.getLatitude();
+            double lng = mLastLocation.getLongitude();
+
+            //LatLng loc = new LatLng(-6.181163, 106.905325);
+            loc = new LatLng(lat, lng);
+        }
+        else
+        {
+            loc = new LatLng(-6.181163, 106.905325);
+        }
+
+        LatLng guru1 = new LatLng(-6.178591, 106.904984);
+        LatLng guru2 = new LatLng(-6.182824, 106.906823);
+        LatLng guru3 = new LatLng(-6.176956, 106.901877);
+
+        mMap.addMarker(new MarkerOptions().position(guru1).title("Guru 1"));
+        mMap.addMarker(new MarkerOptions().position(guru2).title("Guru 2"));
+        mMap.addMarker(new MarkerOptions().position(guru3).title("Guru 3"));
+        mMap.animateCamera(CameraUpdateFactory.newLatLngZoom(loc,15));
     }
 
     private void getLocationPermission()
     {
         if (ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_FINE_LOCATION) != PackageManager.PERMISSION_GRANTED && ActivityCompat.checkSelfPermission(this, Manifest.permission.ACCESS_COARSE_LOCATION) != PackageManager.PERMISSION_GRANTED)
         {
-            ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION},REQUEST_ACCESS_FINE_LOCATION);
+            ActivityCompat.requestPermissions(this, new String[]{ACCESS_FINE_LOCATION}, REQUEST_ACCESS_FINE_LOCATION);
             return;
         }
         mMap.setMyLocationEnabled(true);
+    }
+
+    @Override
+    public void onConnectionSuspended(int i) {
+
+    }
+
+    @Override
+    public void onConnectionFailed(@NonNull ConnectionResult connectionResult) {
+
     }
 
     /**
